a9a2a78d988e94cf2c00b6f44c15234ae9d06a29
Yaroslav <yomelianenkowork@gmail.com>
2023-06-02 13:30:57 +0300

    module-4-lesson-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { Layout } from "./components/Layout";
-import { Posts } from "./components/Posts/Posts";
+import { Counter } from "./components/Counter";
+// import { useState } from "react";
+import { LoginForm } from "./components/LoginForm/LoginForm";
 
 
 
 export const App = () => {
 
+  // const [counter, setCounter] = useState(15)
+
   return (
     <Layout>
-      <Posts />
-
+      {/* <button className="btn btn-primary" onClick={() => setCounter(prev => prev + 1)}>{counter}</button> */}
+      <LoginForm />
+      <Counter defaultAndroid={0}  />
       <ToastContainer />
     </Layout>
   );
